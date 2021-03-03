cec0ae78a77f6691a3786393ddf210a19b617a67
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-03-03 10:33:01 +0100

    ok

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,5 +1,20 @@
-public class Komplex{
-   int a = name;
-   int b = number;
+public class Komplex {
+   public double re;
+   public double im;
 
+   public Komplex(double rik, double hitt) {
+      this.re = rik;
+      this.im = hitt;
+   }
+
+   @Override
+   public String toString() {
+      return re + "+" + im + "i"; //skriv ut talet
+   }
+
+   public Komplex add(Komplex out) {
+      double real = re + out.im;
+      double fake = re + out.im;
+      return new (real, fake);
+   }
 }
